6f8c6cba0688ba5192fe0a46cded8e474fd221a5
Shadowed <shadowed.wow@gmail.com>
2010-02-13 13:01:08 -0800

    Added blacklisting of all enchanting materials looted from mobs while in a group and when loot is not FFA

--- MMOC_Recorder.lua
+++ MMOC_Recorder.lua
@@ -15,7 +15,7 @@ local NPC_TYPES = {["mailbox"] = 0x01, ["auctioneer"] = 0x02, ["battlemaster"] =
 local BATTLEFIELD_TYPES = {["av"] = 1, ["wsg"] = 2, ["ab"] = 3, ["nagrand"] = 4, ["bem"] = 5, ["all_arenas"] = 6, ["eots"] = 7, ["rol"] = 8, ["sota"] = 9, ["dalaran"] = 10, ["rov"] = 11, ["ioc"] = 30, ["all_battlegrounds"] = 32}
 local BATTLEFIELD_MAP = {[L["Alterac Valley"]] = "av", [L["Warsong Gulch"]] = "wsg", [L["Eye of the Storm"]] = "eots", [L["Strand of the Ancients"]] = "sota", [L["Isle of Conquest"]] = "ioc", [L["All Arenas"]] = "all_arenas"}
 -- Items to ignore when looted in a *regular* way
-local IGNORE_LOOT = {[34055] = true}
+local IGNORE_LOOT = {[11082] = true, [34055] = true, [16203] = true, [10939] = true, [11135] = true, [11175] = true, [22446] = true, [10998] = true, [34056] = true, [16202] = true, [10938] = true, [11134] = true, [11174] = true, [2244] = true}
 -- Daze
 local SPELL_BLACKLIST = {[1604] = true}
 
@@ -887,6 +887,8 @@ function Recorder:LOOT_OPENED()
 	
 	if( not npcData ) then return end
 	npcData.looted = (npcData.looted or 0) + 1
+	
+	local inGroup = GetNumPartyMembers() > 0 or GetNumRaidMembers() > 0
 		
 	for i=1, GetNumLootItems() do
 		-- Parse out coin
@@ -903,7 +905,7 @@ function Recorder:LOOT_OPENED()
 		elseif( LootSlotIsItem(i) ) then
 			local link = GetLootSlotLink(i)
 			local itemID = link and tonumber(string.match(link, "item:(%d+)"))
-			if( itemID and ( not isMob or not IGNORE_LOOT[itemID] ) ) then
+			if( itemID and ( not isMob or ( not inGroup or GetLootMethod() == "freeforall" or not IGNORE_LOOT[itemID] ) ) ) then
 				local quantity = select(3, GetLootSlotInfo(i))
 				
 				-- If we have an NPC ID then associate the npc with dropping that item
